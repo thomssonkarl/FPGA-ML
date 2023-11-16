9e872640c6dfa892915bed2dc5c0ae3259c55e7f
Oliver Kiss <oliver.robert1999@gmail.com>
2023-11-16 13:02:52 +0100

    skeleton for feedforward member function

--- models/network.hpp
+++ models/network.hpp
@@ -4,6 +4,7 @@
 //
 #include <random>
 #include <iostream>
+#include <math.h>
 #define LAYERS 3
 
 
@@ -17,10 +18,27 @@ static double f_rand(double f_min, double f_max) {
     return f_min + f * (f_max - f_min);
 }
 
+static double *sigmoid(double *z) {
+    // TODO
+    return 1.0 / (1.0 + exp(-z));
+}
+
+static double *sigmoid_prime(double *z) {
+    // TODO
+    return sigmoid(z)*(1-sigmoid(z));
+}
+
+static double *dot_product(double *a, double *b) {
+    // TODO
+    double res[2] = {0.0, 0.0};
+    return res;
+}
+
 class Network {
     public:
         double *biases[LAYERS-1]; 
         double *weights[LAYERS-1];
+
         Network() {
             for(int i=0; i < LAYERS-1; ++i) {
                 for(int j=0; j < SIZES[i]; ++j) {
@@ -32,6 +50,23 @@ class Network {
                     biases[i][j] = f_rand(SIZES[i],1);
                 }
             }
+        }
+
+        double feedforward(double *input) {
+            for(int i=0; i < LAYERS; ++i) {
+                double *b = biases[i];
+                double *w = weights[i];
+                double res[SIZES[i]];
+
+                double dot_product = dot_product(w, input);
+                
+                for(int j=0; j < SIZES[i + 1]; ++j) { //wrap SIZES?
+                    res[j] = dot_product + b[j];
+                }
+
+                input = sigmoid(res);
+            }
+            return input;
         }
 
         void show() {
