8d1d8e805e553ecd0eb0a28d2096e65811609c6b
bombdoge77 <oliver.robert1999@gmail.com>
2023-11-27 16:10:15 +0100

    skeleton for feedforward

--- inference/inference.c
+++ inference/inference.c
@@ -4,21 +4,42 @@
 
 #define WEIGHTS_SIZE 784
 #define LAYERS_SIZE 2
+#define BIAS_WIDTH 1
 
-double sigmoid(double x) {
-    return 1.0 / (1.0 + exp(-x));
+double sigmoid_single(double z) {
+	return 1.0 / (1.0 + exp(-z));
 }
 
-double dp(double **a, double b) {
-	
+void sigmoid(double **x, double **res) {
+	int height;
+	for(int i=0; i < BIAS_WIDTH; i++) {
+		height = sizeof(x[i]) / sizeof(double);
+		for(int j=0; j < height; j++) {
+			res[i][j] = sigmoid_single(x[i][j]);
+		}
+    	}
+}
+
+double dp(double **a, double **b) {
+	return 0;
+}
+
+void vector_add(double a, double **b, double **add_res) {
+	add_res = b;
 }
 
 int feed_forward(double * in) {
+	double **res = {{1}}; //TODO
+	double **add_res = {{1}}; //TODO
 	for (int layer = 0; layer < LAYERS_SIZE; layer++) {
 		double **bias = biases[layer]; //kolonnvektor
 		double **weight = weights[layer];
 		//dot product
+		double dot = dp(weight, res);
+		vector_add(dot, bias, add_res);
+		sigmoid(add_res, res);
 	}
+	return res[0][0]; //dont worry about this
 }
 
 
@@ -27,7 +48,7 @@ int main() {
     int actual = test_label[0];
     double *input = test_image[0];
     int input_size = WEIGHTS_SIZE; 
-    int output = feed_forward(input, input_size);
+    int output = feed_forward(input);
 
     printf("Output from NN: %d\n", output);
     printf("Actual: %d\n", actual);
