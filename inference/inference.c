d94db0b53866508be2ed6d55c5557ebcc10d29e0
bombdoge77 <oliver.robert1999@gmail.com>
2023-11-27 15:44:22 +0100

    started inference

--- inference/inference.c
+++ inference/inference.c
@@ -3,12 +3,23 @@
 #include <math.h>
 
 #define WEIGHTS_SIZE 784
+#define LAYERS_SIZE 2
 
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
 
+double dp(double **a, double b) {
+	
+}
 
+int feed_forward(double * in) {
+	for (int layer = 0; layer < LAYERS_SIZE; layer++) {
+		double **bias = biases[layer]; //kolonnvektor
+		double **weight = weights[layer];
+		//dot product
+	}
+}
 
 
 int main() {
