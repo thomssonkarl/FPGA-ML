35e498c62668193aa07b92517eccda4930954959
thomssonkarl <thomsson_99@hotmail.com>
2023-11-27 16:09:16 +0100

    Minor fixes

--- inference/inference.c
+++ inference/inference.c
@@ -1,17 +1,25 @@
 #include "../mnistviewer/mnist.h"
 #include "parameters.h"
+#include <math.h>
 
-int feed_forward(double *input) {
-    
+#define WEIGHTS_SIZE 784
+
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
 }
 
+
+
+
 int main() {
-
-    const double *input = test_image[0];
-
-    int output = feed_forward(input);
+    load_mnist();
+    int actual = test_label[0];
+    double *input = test_image[0];
+    int input_size = WEIGHTS_SIZE; 
+    int output = feed_forward(input, input_size);
 
     printf("Output from NN: %d\n", output);
+    printf("Actual: %d\n", actual);
 
     return 0;
 }
