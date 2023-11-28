1810dd97d73746ac6dec1c3ba0f82c95722a6cda
thomssonkarl <thomsson_99@hotmail.com>
2023-11-28 16:39:28 +0100

    Probably working NN in C

--- inference/inference.c
+++ inference/inference.c
@@ -2,25 +2,20 @@
 #include "parameters.h"
 #include <math.h>
 
-#define WEIGHTS_SIZE 784
+#define INPUT_SIZE 784
 #define LAYERS_SIZE 2
 #define BIAS_WIDTH 1
 #define LAYER_1_HEIGHT 30
 #define LAYER_2_HEIGHT 10
-#define HEIGHTS {LAYER_1_HEIGHT, LAYER_2_HEIGHT}
+#define SIZES {LAYER_1_HEIGHT, LAYER_2_HEIGHT}
 
 
 double sigmoid_single(double z) {
-	//TODO: Usually is called with very high/low value of z resulting in exp = 0.0
-	printf("Called with: %f\n", z);
-	double result = 1.0 / (1.0 + exp(-z));
-
-	printf("Returning: %e\n", result);
-	return result;
+	return 1.0 / (1.0 + exp(-z));
 }
 
 
-void sigmoid(double vec[][WEIGHTS_SIZE], double res[][WEIGHTS_SIZE], int height) {
+void sigmoid(double vec[][LAYER_1_HEIGHT], double res[][INPUT_SIZE], int height) {
 	for(int i=0; i < BIAS_WIDTH; i++) {
 		for(int j=0; j < height; j++) {
 			double sig = sigmoid_single(vec[i][j]);
@@ -29,55 +24,94 @@ void sigmoid(double vec[][WEIGHTS_SIZE], double res[][WEIGHTS_SIZE], int height)
     }
 }
 
-double dot_product(double **weights, double *image) {
+void dot_product(double **weights, double *image, double res_vec[][1], int height, int iter) {
 	double result = 0.0;
-	for (int i = 0; i < WEIGHTS_SIZE; i++) {
-		result += weights[0][i] * image[i]; 
+	int size;
+	if (iter == 0) {
+		size = INPUT_SIZE;
 	}
-	return result;
-}
-
-void vector_add(double a, double **b, double add_res[][WEIGHTS_SIZE], int height) {
-	for (int i = 0; i < BIAS_WIDTH; i++) {
-		for (int j = 0; j < height; j++) {
-			add_res[i][j] = b[i][j] + a;
+	if (iter == 1) {
+		size = LAYER_1_HEIGHT;
+	}
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < size; j++) {
+			result += weights[i][j] * image[j]; 
 		}
+		res_vec[i][0] = result;
+		result = 0.0;
 	}
 }
 
-void show_result_vec(double res[BIAS_WIDTH][WEIGHTS_SIZE]) {
+void vector_add(double a[LAYER_1_HEIGHT][1], double **b, double add_res[LAYER_1_HEIGHT][1], int height) {
+	for (int j = 0; j < height; j++) {
+		add_res[j][0] = b[j][0] + a[j][0];
+	}
+}
+
+void show_result_vec(double res[BIAS_WIDTH][INPUT_SIZE]) {
 	for (int i = 0; i < BIAS_WIDTH; i++) {
-		for (int j = 0; j < WEIGHTS_SIZE; j++) {
+		for (int j = 0; j < LAYER_1_HEIGHT; j++) {
 			printf("%e\n", res[i][j]);
 		}
 	}
 }
 
+
+void show_add_vec(double add_vec[LAYER_1_HEIGHT][1]) {
+	for(int i = 0; i < 30; i++) {
+		printf("%e\n", add_vec[i][0]);
+	}
+}
+
+
+int argmax(const double arr[], int size) {
+	if (size <= 0) {
+		fprintf(stderr, "Error: Invalid array size\n");
+		return -1;
+	}
+
+	int max_index = 0;
+	double max_value = arr[0];
+	for (int i = 1; i < size; i++) {
+		if (arr[i] > max_value) {
+			max_value = arr[i];
+			max_index = i;
+		}
+	}
+	return max_index;
+}
+
 int feed_forward(double *input) {
-	double res[BIAS_WIDTH][WEIGHTS_SIZE] = {{0}};
-	double add_res[BIAS_WIDTH][WEIGHTS_SIZE] = {{0}};
-	int heights[] = HEIGHTS;
+	double res[BIAS_WIDTH][INPUT_SIZE] = {{0}};
+	double add_res[LAYER_1_HEIGHT][BIAS_WIDTH] = {{0}};
+	double dp_res_vec[LAYER_1_HEIGHT][1] ={{0}};
+	int heights[] = SIZES;
 
 	for (int layer = 0; layer < LAYERS_SIZE; layer++) {
 		int LAYER_HEIGHT = heights[layer];
 		double **bias = biases[layer];
 		double **weight = weights[layer];
 
-		double dp = dot_product(weight, input);
-		vector_add(dp, bias, add_res, LAYER_HEIGHT);
+		if (layer == 0) {
+			dot_product(weight, input, dp_res_vec, LAYER_HEIGHT, layer);
+		}
+		else if (layer == 1) {
+			dot_product(weight, res[0], dp_res_vec, LAYER_HEIGHT, layer);
+		}
+		vector_add(dp_res_vec, bias, add_res, LAYER_HEIGHT);
 		sigmoid(add_res, res, LAYER_HEIGHT);
-
 	}
-	show_result_vec(res);
-	return res[0][0];
+
+	int output = argmax(res[0], 10);
+	return output;
 }
 
 
 int main() {
 
     load_mnist();
-    int actual = test_label[0];
-    double *input = test_image[0];
+    int actual = test_label[34];
+    double *input = test_image[34];
 
     int output = feed_forward(input);
 
